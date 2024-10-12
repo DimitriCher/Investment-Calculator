24bcec11a7a9c9e886eef6dc9b455ddc4f7cc537
DimitriChernyshev <dimitri.chernyshev@gmail.com>
2024-10-12 14:03:02 +0200

    Added two way binding

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,63 @@
+import { useState } from 'react';
+
 export default function UserInput() {
+  const [UserInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIndentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIndentifier]: newValue };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label id="user-input label">Initial Investment</label>
-          <input id="user-input input" type="number" required></input>
+          <input
+            id="user-input input"
+            type="number"
+            required
+            value={UserInput.initialInvestment}
+            onChange={(e) => handleChange('initialInvestment', e.target.value)}
+          ></input>
         </p>
         <p>
           <label id="user-input label">Annual Investment</label>
-          <input id="user-input input" type="number" required></input>
+          <input
+            id="user-input input"
+            type="number"
+            required
+            value={UserInput.annualInvestment}
+            onChange={(e) => handleChange('annualInvestment', e.target.value)}
+          ></input>
         </p>
       </div>
       <div className="input-group">
         <p>
           <label id="user-input label">Expected Return</label>
-          <input id="user-input input" type="number" required></input>
+          <input
+            id="user-input input"
+            type="number"
+            required
+            value={UserInput.expectedReturn}
+            onChange={(e) => handleChange('expectedReturn', e.target.value)}
+          ></input>
         </p>
         <p>
           <label id="user-input label">Duration</label>
-          <input id="user-input input" type="number" required></input>
+          <input
+            id="user-input input"
+            type="number"
+            required
+            value={UserInput.duration}
+            onChange={(e) => handleChange('duration', e.target.value)}
+          ></input>
         </p>
       </div>
     </section>
